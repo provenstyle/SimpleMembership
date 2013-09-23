20d1544e7a51f4ad47dbe1fcce1107494e2e2a56
Michael Dudley <michael.dudley@improvingenterprises.com>
2013-09-22 20:27:41 -0500

    Initial commit

---
+++ SimpleMembership/Role.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProvenStyle.Core.Web
+{
+    [Table("webpages_Roles")]
+    public class Role
+    {
+        public Role()
+        {
+            Members = new List<Membership>();
+        }
+
+        [Key]
+        public int RoleId { get; set; }
+
+        [StringLength(256)]
+        public string RoleName { get; set; }
+
+        public ICollection<Membership> Members { get; set; }
+    }
+}
